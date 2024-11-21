47291f209fbe7f873b2241ad39b3c2c31e4d36f0
Simon Highfield <highfieldsimon@gmail.com>
2024-11-21 20:34:00 +0000

    created custom Login component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { supabase } from './supabaseClient'
 import { createSupabaseSession } from './createSupabaseSession'
 import { handleSignOut } from './handleSignOut'
+import Login from './components/Login'
 
 function App() {
   const [session, setSession] = useState(null)
@@ -15,7 +16,8 @@ function App() {
   }, [])
 
   if (!session) {
-    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }}/>)
+    // return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }}/>)
+    return (<Login/>)
   }
   else {
     return (
---
+++ src/components/Login.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import { supabase } from '../supabaseClient'
+
+export default function Login() {
+  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    setLoading(true)
+    
+    await supabase.auth.signInWithPassword({ 
+        email: email,
+        password: password
+    })
+
+    if (error) {
+      alert(error.error_description || error.message)
+    } 
+    
+    setLoading(false)
+  }
+
+  return (
+        <>
+            <p className="description">Sign in with your email below</p>
+            <form onSubmit={handleLogin}>
+                <div>
+                    <input
+                    className="inputField"
+                    type="email"
+                    placeholder="Your email"
+                    value={email}
+                    required={true}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input
+                    className="inputField"
+                    type="password"
+                    placeholder="Your password"
+                    value={password}
+                    required={true}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <button disabled={loading}>
+                    {loading ? <span>Loading</span> : <span>Login</span>}
+                    </button>
+                </div>
+            </form>
+        </>
+    )
+}
