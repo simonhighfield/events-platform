62ea08263c19bdeffeecb73477805dc2c2e544de
Simon Highfield <highfieldsimon@gmail.com>
2024-11-29 19:19:26 +0000

    Created <Routes> and pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,14 @@ import { supabase } from './utils/supabaseClient'
 import { createSupabaseSession } from './utils/createSupabaseSession'
 import { handleSignOut } from './utils/handleSignOut'
 import { addCurrentUserToPublicUserProfiles } from './utils/addCurrentUserToPublicUserProfiles'
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import NoPage from './components/NoPage';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 
 function App() {
   const [session, setSession] = useState(null)
@@ -15,17 +23,31 @@ function App() {
     return createSupabaseSession(setSession)
   }, [])
 
-  if (!session) {
-    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]}/>)
-  } else {
-    return (
-      <>
-        <div>Logged in!</div>
-        <button onClick={handleSignOut}>sign out</button>
-        <button onClick={() => addCurrentUserToPublicUserProfiles(session)}>add user to publicprofiles</button>
-      </>
-    )
-  }
+  return (
+    <>
+      <NavBar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path="login" element={<LoginPage/>} />
+          <Route path="*" element={<NoPage/>} />
+        </Routes>
+      </BrowserRouter>
+      <Footer/>
+    </>    
+  )
+  
+  // if (!session) {
+  //   return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]}/>)
+  // } else {
+  //   return (
+  //     <>
+  //       <div>Logged in!</div>
+  //       <button onClick={handleSignOut}>sign out</button>
+  //       <button onClick={() => addCurrentUserToPublicUserProfiles(session)}>add user to publicprofiles</button>
+  //     </>
+  //   )
+  // }
 }
 
 export default App
