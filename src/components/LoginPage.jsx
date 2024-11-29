7e4e8b8481463b24336a9f716d71e22e7e11e8ea
Simon Highfield <highfieldsimon@gmail.com>
2024-11-29 19:31:52 +0000

    Moved sign-in functionality to LoginPage.jsx, passing it session via props

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,5 +1,20 @@
-export default function LoginPage () {
-    return(
-        <h1>LoginPage.jsx</h1>
-    )
+import { supabase } from '../utils/supabaseClient'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { handleSignOut } from "../utils/handleSignOut"
+import { addCurrentUserToPublicUserProfiles } from "../utils/addCurrentUserToPublicUserProfiles"
+
+export default function LoginPage ({ session }) {
+
+    if (!session) {
+        return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]}/>)
+      } else {
+        return (
+          <>
+            <div>Logged in!</div>
+            <button onClick={handleSignOut}>sign out</button>
+            <button onClick={() => addCurrentUserToPublicUserProfiles(session)}>add user to publicprofiles</button>
+          </>
+        )
+      }
 }
