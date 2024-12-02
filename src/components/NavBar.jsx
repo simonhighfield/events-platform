96792e3bdbbc328f0551f3f3128a21d047a29dbb
Simon Highfield <highfieldsimon@gmail.com>
2024-12-02 16:46:48 +0000

    NavBar.jsx contains <Link>s to the <Route>s: <HomePage> and <LoginPage>

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom"
+
 export default function NavBar () {
     return(
-        <h1>NavBar.jsx</h1>
+        <nav style={{backgroundColor: 'grey'}}>
+            <h1>NavBar.jsx</h1>
+            <Link to={`/`}>Home</Link><span> </span>
+            <Link to={`login`}>login</Link>
+        </nav>
     )
 }
