11ac037be34fb69ee80fe3db35325e42fe0a828b
Simon Highfield <highfieldsimon@gmail.com>
2024-12-03 19:16:05 +0000

    Imported skiddleApiKey from .env

--- src/utils/fetchSkiddleEvents.js
+++ src/utils/fetchSkiddleEvents.js
@@ -1,5 +1,6 @@
 import axios from "axios";
+const skiddleApiKey = import.meta.env.VITE_SKIDDLE_KEY
 
 export default function fetchSkiddleEvents () {
-    console.log('fetching skiddle events');
+    console.log('fetching skiddle events with: ', skiddleApiKey);
 }
