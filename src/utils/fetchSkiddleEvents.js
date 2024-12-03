8ee66e9c46fc3e4bdbd0e90f35ac1efbbf519fe9
Simon Highfield <highfieldsimon@gmail.com>
2024-12-03 19:51:10 +0000

    Can successfully fetch CLUB events in Manchester

--- src/utils/fetchSkiddleEvents.js
+++ src/utils/fetchSkiddleEvents.js
@@ -1,6 +1,27 @@
 import axios from "axios";
-const skiddleApiKey = import.meta.env.VITE_SKIDDLE_KEY
 
 export default function fetchSkiddleEvents () {
-    console.log('fetching skiddle events with: ', skiddleApiKey);
+    const skiddleApiKey = import.meta.env.VITE_SKIDDLE_KEY
+    const latitude = 53.4839
+    const longitude = -2.446
+    const radius = 10
+    const limit = 10
+    const eventcode = 'CLUB';
+
+
+    return axios.get(`https://www.skiddle.com/api/v1/events/search/?api_key=${skiddleApiKey}/&`,
+        { params: {
+                latitude,
+                longitude,
+                radius,
+                eventcode,
+                limit,
+                description:true
+            },
+        }
+    )
+    .then(({ data }) => {
+        console.log(data);
+        // return data.results;
+    });
 }
