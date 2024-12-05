3f702a2f56f24ff4bc437ebcd3d531fef4b6a64a
Simon Highfield <highfieldsimon@gmail.com>
2024-12-05 21:02:01 +0000

    Created test data

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,13 @@
 import { useContext, useState } from 'react'
-import { SessionContext } from '../Contexts'
+import { ProfileContext, SessionContext } from '../Contexts'
 import SessionId from './SessionId';
 import fetchAllEvents from '../utils/fetchAllEvents';
 import skiddleParamsForClubEventsInManchester from '../data/skiddleParamsForClubEventsInManchester';
+import { postAdminEvent } from '../utils/postAdminEvent';
+import paramsForTestAdminEvent from '../data/paramsForTestAdminEvent';
 
 export default function HomePage () {
+    const { profile } = useContext(ProfileContext)
 
     function handleFetchAllEvents () {
         fetchAllEvents(skiddleParamsForClubEventsInManchester)
@@ -15,11 +18,17 @@ export default function HomePage () {
         })
     }
 
+    function handlePostAdminEvent() {
+        paramsForTestAdminEvent.admin_id = profile.id        
+        console.log(paramsForTestAdminEvent);
+    }
+
     return(
         <>
             <h1>HomePage.jsx</h1>
             <SessionId/>
             <button onClick={handleFetchAllEvents}>get all events</button>
+            <button onClick={handlePostAdminEvent}>post test admin event</button>
         </>
     )
 }
---
+++ src/data/paramsForTestAdminEvent.js
@@ -0,0 +1,12 @@
+const paramsForTestAdminEvent = {
+    admin_id: null,
+    event_name: 'test admin event',
+    event_date: new Date('2024-12-20'),
+    location: 'Manchester',
+    contributors: ['DJ 1', 'DJ 2'],
+    event_photo_url: 'https://images.unsplash.com/photo-1620909868342-5659c05ab5e5?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    descriptionL: 'The coolest party',
+    additional_data: null
+}
+
+export default paramsForTestAdminEvent
