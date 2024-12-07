a55dfcc65df84aafe7c02b09ff36080b62563ea9
Simon Highfield <highfieldsimon@gmail.com>
2024-12-07 20:32:53 +0000

    created functions to generate formatted dates to filter the fetchAdminEvents supabase query

--- src/utils/fetchAdminEvents.js
+++ src/utils/fetchAdminEvents.js
@@ -1,6 +1,16 @@
 import { supabase } from "./supabaseClient";
+import { getDateTodayInYYYYMMDD } from "./getDateTodayInYYYYMMDD";
+import { getDatePlusThirtyInYYYYMMDD } from "./getDatePlusThirtyInYYYYMMDD";
 
-export default async function fetchAdminEvents(limit) {
+export default async function fetchAdminEvents(startDate, endDate, limit) {
+
+    const searchFromDate = startDate || getDateTodayInYYYYMMDD()
+    console.log('search from', searchFromDate);
+
+    const searchUntilDate = endDate || getDatePlusThirtyInYYYYMMDD(searchFromDate)
+    console.log('until', searchUntilDate);
+    
+
     const { data, error } = await supabase
         .from('admin_events')
         .select()
