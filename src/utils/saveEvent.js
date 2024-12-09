3aea4e63c73522e2f26ef1ce2b8ef3866a882bf2
Simon Highfield <highfieldsimon@gmail.com>
2024-12-09 21:20:51 +0000

    saveEvent now destructures admin_event_id and skiddle_event_id

--- src/utils/saveEvent.js
+++ src/utils/saveEvent.js
@@ -1,22 +1,22 @@
 import { supabase } from "./supabaseClient";
 
-export default async function saveEvent(profile, event) {
+export default async function saveEvent(profile, { admin_event_id, skiddle_event_id}) {
 
-    const event_source = (function getEventSource(event) {
-        if (event.admin_event_id) {
+    const event_source = (function getEventSource() {
+        if (admin_event_id) {
             return 'admin'
-        } else if (event.skiddle_event_id) {
+        } else if (skiddle_event_id) {
             return 'skiddle'
         }
         return null
-    })(event)
+    })()
 
     const { data, error } = await supabase
         .from('saved_events')
         .insert({
             user_id: profile.id,
             event_source,
-            admin_event_id: '03b0ec4a-8222-4a50-845f-1dc5169f8e93',
+            admin_event_id
         })
         .select()
     if (data) {
