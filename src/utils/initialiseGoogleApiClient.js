6ef1b756295e1aa6ef8d3a95894682499d660ca5
Simon Highfield <highfieldsimon@gmail.com>
2024-12-12 15:08:08 +0000

    refactored initialiseGoogleApiClient

--- src/utils/initialiseGoogleApiClient.js
+++ src/utils/initialiseGoogleApiClient.js
@@ -1,9 +1,9 @@
 import { gapi } from "gapi-script";
 
-const API_KEY = import.meta.env.VITE_GOOGLE_CAL_API_KEY;
-const CLIENT_ID = import.meta.env.VITE_GOOGLE_CAL_CLIENT_ID;    // Authorisation credentials for web application
-const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'];  // Discovery doc URL for APIs used by the quickstart
-const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'; // Authorization scopes
+const apiKey = import.meta.env.VITE_GOOGLE_CAL_API_KEY;
+const client_id = import.meta.env.VITE_GOOGLE_CAL_CLIENT_ID;    // Authorisation credentials for web application
+const discoveryDocs = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'];  // For APIs used by the quickstart
+const scope = 'https://www.googleapis.com/auth/calendar.readonly'; // Authorization scopes
 
 export default function initialiseGoogleApiClient() {
     
@@ -13,10 +13,10 @@ export default function initialiseGoogleApiClient() {
 
     function initialiseApiJsClientLibrary() {
         gapi.client.init({
-            apiKey: API_KEY,
-            client_id: CLIENT_ID,
-            discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-            scope: SCOPES,
+            apiKey,
+            client_id,
+            discoveryDocs,
+            scope
         });
     }
 }
