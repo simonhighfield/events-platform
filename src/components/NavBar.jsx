13baa7533fb93b3eaeef2e4ef4b83bf0141b3f0e
Simon Highfield <highfieldsimon@gmail.com>
2024-12-13 15:13:26 +0000

    Replaced <nav> with react-bs <Nav>. Retained <Links> as they are quicker than react-bs <Nav Links>

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,28 @@
 import { useContext } from "react"
 import { Link } from "react-router-dom"
 import { ProfileContext } from "../Contexts"
+import Nav from 'react-bootstrap/Nav';
+import "./NavBar.css"
 
-export default function NavBar () {
+
+export default function NavBar() {
     const { profile } = useContext(ProfileContext)
-    
-    return(
-        <nav style={{backgroundColor: 'grey'}}>
-            <h1>NavBar.jsx</h1>
-            <Link to={`/`}>Home</Link><span> </span>
-            <Link to={`login`}>login</Link>
-            {profile ? (<p>signed in as {profile.username}</p>) : <p>not signed in</p>}
-        </nav>
+
+    return (
+        // <nav style={{backgroundColor: 'grey'}}>
+        //     <h1>NavBar.jsx</h1>
+        //     <Link to={`/`}>Home</Link><span> </span>
+        //     <Link to={`login`}>login</Link>
+        //     {profile ? (<p>signed in as {profile.username}</p>) : <p>not signed in</p>}
+        // </nav>
+
+        <Nav className="justify-content-between">
+            <Nav.Item>
+                <Link to={`/`}>Home</Link><span> </span>
+            </Nav.Item>
+            <Nav.Item>
+                <Link to={`login`}>profile</Link><span> </span>
+            </Nav.Item>
+        </Nav>
     )
 }
