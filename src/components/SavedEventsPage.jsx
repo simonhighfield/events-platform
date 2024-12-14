c4de4a33a048c51e0fed6458364599726a8eab11
Simon Highfield <highfieldsimon@gmail.com>
2024-12-14 21:07:01 +0000

    fetching profile is async (from supabase listener) so use profile state to return a login prompt or a saved events header

--- src/components/SavedEventsPage.jsx
+++ src/components/SavedEventsPage.jsx
@@ -1,25 +1,47 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { ProfileContext } from "../Contexts"
+import fetchSavedEventsByUserId from "../utils/fetchSavedEventsByUserId"
+import Loading from "./Loading"
+// import EventsFeed from "./EventsFeed"
 
 export default function Login() {
-    const { profile } = useContext(ProfileContext)    
+    const { profile } = useContext(ProfileContext)
+    const [ eventsFound, setEventsFound ] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
 
-    // useEffect(() => {
-    //     fetchAndSortAllEvents(skiddleParamsForClubEventsInManchester)
-    //     .then(({ events }) => {
-    //         setEventsFound(events)
-    //     })
-    //     .catch(({error}) => {
-    //     })
-    //     .finally(() => {
-    //         setIsLoading(false)
-    //     })
-    // }, [])
+    useEffect(() => {
+
+        if (profile) {
+            setIsLoading(true)
+            fetchSavedEventsByUserId(profile.id)
+            .then(({ savedEvents }) => {
+                console.log(savedEvents);
+                setEventsFound(savedEvents)
+            })
+            .catch(({error}) => {
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
+        }
+    }, [profile])
+
+
+    if (!profile) {
+        return (
+            <main className='responsive-page-sizing'>
+                <p>Please sign in to view your saved events</p>
+            </main>
+        )
+    }
 
     return (
         <main className='responsive-page-sizing'>
-           <h1>SavedEventsPage.jsx</h1>
-           <p>{profile.id}</p>
+            {isLoading
+                ? <Loading/>
+                : <h2>saved events</h2>
+                // : <EventsFeed events={eventsFound} />
+            }
         </main>
     )
 }
