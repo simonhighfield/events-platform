f72a4ebc29c28d60d2b98bf48f906a4f1eeb8e81
Simon Highfield <highfieldsimon@gmail.com>
2024-12-14 20:39:40 +0000

    SavedEventsPage uses ProfileContext to get profile.id

--- src/components/SavedEventsPage.jsx
+++ src/components/SavedEventsPage.jsx
@@ -1,8 +1,25 @@
+import { useContext } from "react"
+import { ProfileContext } from "../Contexts"
+
 export default function Login() {
+    const { profile } = useContext(ProfileContext)    
+
+    // useEffect(() => {
+    //     fetchAndSortAllEvents(skiddleParamsForClubEventsInManchester)
+    //     .then(({ events }) => {
+    //         setEventsFound(events)
+    //     })
+    //     .catch(({error}) => {
+    //     })
+    //     .finally(() => {
+    //         setIsLoading(false)
+    //     })
+    // }, [])
 
     return (
         <main className='responsive-page-sizing'>
            <h1>SavedEventsPage.jsx</h1>
+           <p>{profile.id}</p>
         </main>
     )
 }
