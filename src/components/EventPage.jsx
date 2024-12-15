51b2af2e3079372d2dbdf5e210b7bad52715279f
Simon Highfield <highfieldsimon@gmail.com>
2024-12-15 12:22:12 +0000

    Added logic to fetch full event data by ID from Admin or Skiddle

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -1,11 +1,43 @@
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
+import fetchSkiddleEventById from "../utils/fetchSkiddleEventById";
+import fetchAdminEventById from '../utils/fetchAdminEventById';
+import formatSkiddleEvent from '../utils/formatSkiddleEvent';
 
 export default function EventPage () {
     const { eventSource, eventId } = useParams();
-    console.log(eventSource, eventId);
-    
+    const [isLoading, setIsLoading] = useState(true)
+    const [eventFetched, setEventFetched] = useState({})
+
+    useEffect(() => {
+        setIsLoading(true)
+        
+        if (eventSource === 'admin') {
+            fetchAdminEventById(eventId)
+            .then(({ event }) => {
+                setEventFetched(event)
+            })
+            .catch(({error}) => {
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
+        } else if (eventSource === 'skiddle') {
+            fetchSkiddleEventById(eventId)
+            .then(({ event }) => {
+                const formattedEvent = formatSkiddleEvent(event)
+                setEventFetched(formattedEvent)
+            })
+            .catch(({error}) => {
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
+        }
+    },[eventId])
+
     return(
-        <h1>event</h1>
+        <h1>{eventFetched.event_name}</h1>
 
     )
 }
