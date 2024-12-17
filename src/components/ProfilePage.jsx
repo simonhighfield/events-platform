6b6915cfd0218aa321570125626c6a114adda550
Simon Highfield <highfieldsimon@gmail.com>
2024-12-17 21:28:13 +0000

    SavedEvents also naviagets to SignIn

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -3,53 +3,54 @@ import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { handleSignOut } from "../utils/handleSignOut"
 import { addCurrentUserToPublicUserProfiles } from "../utils/addCurrentUserToPublicUserProfiles"
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { GoogleTokenContext, SessionContext } from '../Contexts'
 import HelloProfile from './HelloProfile'
 import connectGoogleAccount from '../utils/connectGoogleAccount'
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 import LoadingButton from './LoadingButton';
+import { useLocation, useNavigate } from 'react-router-dom'
 
 
 export default function ProfilePage () {
-    const { session } = useContext(SessionContext);
-    const { token, setGoogleToken } = useContext(GoogleTokenContext)
+  const { session } = useContext(SessionContext);
+  const { token, setGoogleToken } = useContext(GoogleTokenContext)
+  const navigate = useNavigate()
+  const location = useLocation()
 
-    async function handleGoogleSignIn() {
-      const { token } = await connectGoogleAccount()
-      setGoogleToken(token)
-    }
+  useEffect(() => {
+      if (!session) {
+        navigate('/sign-in', {state: {previousUrl: location.pathname}});
+      }
+  },[session])
 
-    if (!session) {
-      return (
-        <main className='responsive-page-sizing'>
-          <HelloProfile/>
-          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]}/>
-        </main>
-      )
-    } else {
-      return (
-        <main className='responsive-page-sizing'>
-          <HelloProfile/>
-          <div className="d-grid gap-2">
-            <LoadingButton
-              asyncFunction={handleGoogleSignIn}
-              initialVariant='success'
-              initialText={'Connect Google Calendar'}
-              successText = {'Google Calendar connected'}
-            />
-            <Button
-              onClick={handleSignOut}
-              variant="danger"
-              size="lg"
-            >
-              Sign out
-            </Button>
-          </div>
-        </main>
-      )
-    }
+  async function handleGoogleSignIn() {
+    const { token } = await connectGoogleAccount()
+    setGoogleToken(token)
+  }
+
+  return (
+    <main className='responsive-page-sizing'>
+      <HelloProfile/>
+      <div className="d-grid gap-2">
+        <LoadingButton
+          asyncFunction={handleGoogleSignIn}
+          initialVariant='success'
+          initialText={'Connect Google Calendar'}
+          successText = {'Google Calendar connected'}
+        />
+        <Button
+          onClick={handleSignOut}
+          variant="danger"
+          size="lg"
+        >
+          Sign out
+        </Button>
+      </div>
+    </main>
+  )
+    
 
 
   // return (
--- src/components/SavedEventsPage.jsx
+++ src/components/SavedEventsPage.jsx
@@ -2,20 +2,29 @@ import { supabase } from "../utils/supabaseClient"
 import { Auth } from "@supabase/auth-ui-react"
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useContext, useEffect, useState } from "react"
-import { ProfileContext } from "../Contexts"
+import { ProfileContext, SessionContext } from "../Contexts"
 import fetchSavedEventsByUserId from "../utils/fetchSavedEventsByUserId"
 import Loading from "./Loading"
 import EventsFeed from "./EventsFeed"
 import { generateSavedEvents } from "../utils/generateSavedEvents"
 import HelloProfile from "./HelloProfile"
+import { useLocation, useNavigate } from "react-router-dom"
 
 export default function Login() {
     const { profile } = useContext(ProfileContext)
+    const { session } = useContext(SessionContext);
     const [ eventsFound, setEventsFound ] = useState(null)
     const [isLoading, setIsLoading] = useState(true)
+    const navigate = useNavigate()
+    const location = useLocation()
 
     useEffect(() => {
+        if (!session) {
+          navigate('/sign-in', {state: {previousUrl: location.pathname}});
+        }
+    },[session])
 
+    useEffect(() => {
         if (profile) {
             setIsLoading(true)
             fetchSavedEventsByUserId(profile.id)
@@ -33,23 +42,12 @@ export default function Login() {
         }
     }, [profile])
 
-
-    if (!profile) {
-        return (
-            <main className='responsive-page-sizing'>
-                <HelloProfile/>
-                <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]}/>
-            </main>
-        )
-    } else {
-        return (
-            <main className='responsive-page-sizing'>
-                {isLoading
-                    ? <Loading/>
-                    : <EventsFeed events={eventsFound} />
-                }
-            </main>
-        )
-    }
-
+    return (
+        <main className='responsive-page-sizing'>
+            {isLoading
+                ? <Loading/>
+                : <EventsFeed events={eventsFound} />
+            }
+        </main>
+    )
 }
