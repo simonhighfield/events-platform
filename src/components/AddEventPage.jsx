bf797991a6567dd8774b5b5b351cbd70bc7cafe9
Simon Highfield <highfieldsimon@gmail.com>
2024-12-21 14:36:59 +0000

    Event Name input updates eventName state, which handlePostAdminEvent uses to cretae eventData { }

--- src/components/AddEventPage.jsx
+++ src/components/AddEventPage.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ProfileContext } from '../Contexts';
 import paramsForTestAdminEvent from '../data/paramsForTestAdminEvent';
 import { postAdminEvent } from '../utils/postAdminEvent';
@@ -9,23 +9,28 @@ import Row from 'react-bootstrap/Row';
 
 export default function AddEventPage() {
     const { profile } = useContext(ProfileContext)
+    const [eventName, setEventName] = useState('');
 
     function handlePostAdminEvent() {
-        paramsForTestAdminEvent.admin_id = profile.id
-        postAdminEvent(paramsForTestAdminEvent)
-        .then(({ event })=> {
-            console.log('successfully posted: ', event);
-        })
-        .catch(({ error }) => {
-            console.log(error);
-        })
+
+        const eventData = {
+            event_name: eventName
+        }
+        console.log(eventData);
+
+        // paramsForTestAdminEvent.admin_id = profile.id
+        // postAdminEvent(paramsForTestAdminEvent)
+        // .then(({ event })=> {
+        //     console.log('successfully posted: ', event);
+        // })
+        // .catch(({ error }) => {
+        //     console.log(error);
+        // })
     }
 
     return (
         <main className='responsive-page-sizing'>
            <h1>Add a new event</h1>
-           {/* <button onClick={handlePostAdminEvent}>post test admin event</button> */}
-
             <Form>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>Event Name</Form.Label>
@@ -33,7 +38,9 @@ export default function AddEventPage() {
                         type="text" 
                         placeholder="Name it something grabby" 
                         size="lg"
-                        />
+                        value={eventName}
+                        onChange={(event) => setEventName(event.target.value)}
+                    />
                 </Form.Group>
                 <Row>
                     <Form.Group as={Col} className="mb-3" controlId="exampleForm.ControlInput1">
@@ -61,7 +68,8 @@ export default function AddEventPage() {
                     <Form.Label>Photo URL</Form.Label>
                     <Form.Control type="url" placeholder="Enter a URL" size="lg"/>
                 </Form.Group>
-            </Form> 
+            </Form>
+           <button onClick={handlePostAdminEvent}>post event</button>
         </main>
     )
 }
