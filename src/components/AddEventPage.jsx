8479acda127dae7767b4296e2b0506f1c30bd5d2
Simon Highfield <highfieldsimon@gmail.com>
2024-12-21 13:13:38 +0000

    Moved postAdminEvent to AddEventPage

--- src/components/AddEventPage.jsx
+++ src/components/AddEventPage.jsx
@@ -1,8 +1,27 @@
+import { useContext } from 'react';
+import { ProfileContext } from '../Contexts';
+import paramsForTestAdminEvent from '../data/paramsForTestAdminEvent';
+import { postAdminEvent } from '../utils/postAdminEvent';
+
 export default function AddEventPage() {
+    const { profile } = useContext(ProfileContext)
+
+    function handlePostAdminEvent() {
+        paramsForTestAdminEvent.admin_id = profile.id
+        postAdminEvent(paramsForTestAdminEvent)
+        .then(({ event })=> {
+            console.log('successfully posted: ', event);
+        })
+        .catch(({ error }) => {
+            console.log(error);
+        })
+    }
+
 
     return (
         <main className='responsive-page-sizing'>
            <h1>AddEventPage.jsx</h1>
+           <button onClick={handlePostAdminEvent}>post test admin event</button>
         </main>
     )
 }
