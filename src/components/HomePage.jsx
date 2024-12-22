2e607b3d700e6081cacf1509982a131297a1f514
Simon Highfield <highfieldsimon@gmail.com>
2024-12-22 18:41:06 +0000

    Removed redunant imports from HomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,27 +1,11 @@
-import { useContext, useEffect, useState } from 'react'
-import { GoogleTokenContext, ProfileContext, SessionContext } from '../Contexts'
+import { useEffect, useState } from 'react'
 import fetchAndSortAllEvents from '../utils/fetchAndSortAllEvents.js';
 import skiddleParamsForClubEventsInManchester from '../data/skiddleParamsForClubEventsInManchester';
-import { postAdminEvent } from '../utils/postAdminEvent';
-import paramsForTestAdminEvent from '../data/paramsForTestAdminEvent';
-import deleteAdminEventById from '../utils/deleteAdminEventById';
-import fetchAdminEventsByDate from '../utils/fetchAdminEventsByDate.js';
-import fetchAdminEvents from '../utils/fetchAdminEvents.js';
-import saveEvent from '../utils/saveEvent.js';
-import paramsForTestSaveEvent from '../data/paramsForTestSaveEvent.js';
-import deleteSavedEventById from '../utils/deleteSavedEventById.js';
-import fetchSavedEventsByUserId from '../utils/fetchSavedEventsByUserId.js';
-import { generateSavedEvents } from '../utils/generateSavedEvents.js';
-import paramsForTestSaveSkiddleEvent from '../data/paramsForTestSaveSkiddleEvent';
-import connectGoogleAccount from '../utils/connectGoogleAccount.js';
-import { addEventToGoogleCalendar } from '../utils/addEventToGoogleCalendar.js';
 import EventsFeed from './EventsFeed.jsx';
 import Loading from './Loading.jsx';
 import HelloProfile from './HelloProfile.jsx';
 
 export default function HomePage () {
-    const { profile } = useContext(ProfileContext)
-    const { googleToken, setGoogleToken } = useContext(GoogleTokenContext)
     const [ eventsFound, setEventsFound ] = useState(null)
     const [isLoading, setIsLoading] = useState(true)
 
