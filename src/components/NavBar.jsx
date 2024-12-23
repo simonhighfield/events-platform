dab9ee7e285c699c6f166d750120a3cdbae002d2
Simon Highfield <highfieldsimon@gmail.com>
2024-12-23 19:52:53 +0000

    Added Image to NavBar, but it does not resize well to mobile screen

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,15 +2,18 @@ import { useContext } from "react"
 import { Link } from "react-router-dom"
 import { ProfileContext } from "../Contexts"
 import Nav from 'react-bootstrap/Nav';
+import Image from 'react-bootstrap/Image';
 import "./NavBar.css"
 
 export default function NavBar() {
     const { profile } = useContext(ProfileContext)
 
     return (
-        <Nav className="NavBar responsive-page-sizing sticky-top justify-content-between">
+        <Nav className="NavBar responsive-page-sizing sticky-top justify-content-between align-items-center">
             <Nav.Item>
-                <Link to={`/`}>Home</Link><span> </span>
+                <Link to={`/`}>
+                <Image src="/whatsOnLogoV4.png" className="h-100 img-fluid d-block" style={{maxHeight: "70px"}} />
+                </Link><span> </span>
             </Nav.Item>
             <Nav.Item>
                 <Link to={`profile`}>Profile</Link><span> </span>
